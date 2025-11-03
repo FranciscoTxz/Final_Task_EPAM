a675d0e5ef2e41fcd5e1bcb86e71e2d801ed22c6
Angel Torres <angel_torres@epam.com>
2025-11-03 15:17:42 -0600

    AWS and RDS working

--- app/crud/aws_crud.py
+++ app/crud/aws_crud.py
@@ -1,28 +1,25 @@
 from fastapi import UploadFile
-# from app.services.aws_setup import s3_client, AWS_BUCKET_NAME
-# import uuid
+from app.services.aws_setup import s3_client, AWS_BUCKET_NAME, AWS_REGION
+import uuid
 
 
 async def upload_file_to_s3(file: UploadFile) -> str:
     """Upload a file to S3 and return the URL"""
     try:
-        # # Generate unique filename
-        # file_extension = file.filename.split(".")[-1]
-        # unique_filename = f"{uuid.uuid4()}.{file_extension}"
+        # Generate unique filename
+        file_extension = file.filename.split(".")[-1]
+        unique_filename = f"{uuid.uuid4()}.{file_extension}"
 
-        # # Upload file
-        # s3_client.upload_fileobj(
-        #     file.file,
-        #     AWS_BUCKET_NAME,
-        #     unique_filename,
-        #     ExtraArgs={"ACL": "public-read"},
-        # )
+        # Upload file
+        s3_client.upload_fileobj(
+            file.file,
+            AWS_BUCKET_NAME,
+            unique_filename,
+            ExtraArgs={"ACL": "public-read"},
+        )
 
-        # # Generate URL
-        # url = f"https://{AWS_BUCKET_NAME}.s3.amazonaws.com/{unique_filename}"
-        # return url
-
-        url = f"https://aws.s3.amazonaws.com/{file.filename}"
+        # Generate URL
+        url = f"https://{AWS_BUCKET_NAME}.s3.{AWS_REGION}.amazonaws.com/{unique_filename}"
         return url
 
     except Exception as e:
@@ -32,11 +29,11 @@ async def upload_file_to_s3(file: UploadFile) -> str:
 async def delete_file_from_s3(url: str) -> bool:
     """Delete a file from S3 using its URL"""
     try:
-        # # Extract key from URL
-        # key = url.split("/")[-1]
+        # Extract key from URL
+        key = url.split("/")[-1]
 
-        # # Delete file
-        # s3_client.delete_object(Bucket=AWS_BUCKET_NAME, Key=key)
+        # Delete file
+        s3_client.delete_object(Bucket=AWS_BUCKET_NAME, Key=key)
         return True
 
     except Exception as e:
--- app/database.py
+++ app/database.py
@@ -1,10 +1,10 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import declarative_base
 import os
+from dotenv import load_dotenv
 
-# DATABASE_URL = "postgresql+asyncpg://postgres:Pato1234@localhost:5432/postgres" # local
-DATABASE_URL = "postgresql+asyncpg://postgres:secret123@db:5432/fastapi_db"
-DATABASE_URL = os.getenv("DATABASE_URL", DATABASE_URL)
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL", "ERROR")
 
 engine = create_async_engine(DATABASE_URL)
 
